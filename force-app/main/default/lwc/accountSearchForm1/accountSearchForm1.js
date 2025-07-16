08e091a4e5beca5242c311464f30fae28547270f
Sachin Kr Pradhan <sachinpradhan.crm@gmail.com>
2025-07-16 12:29:18 +0530

    change the branch test

--- force-app/main/default/lwc/accountSearchForm1/accountSearchForm1.js
+++ force-app/main/default/lwc/accountSearchForm1/accountSearchForm1.js
@@ -3,7 +3,7 @@ import { LightningElement } from 'lwc';
 export default class AccountSearchForm1 extends LightningElement {
     searchText = '';
     accountNameChangeHandler(event){
-        this.searchText = event.target.value;
+        this.searchText = event.target.value;//test
     }
     handleClick(){
         //console.log('Search for ' + this.searchText);
